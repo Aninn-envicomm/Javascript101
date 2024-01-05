3fea88b65025825c19f9c52c1740a6831f54c105
Aninn-envicomm <envicomm.nina@gmail.com>
2024-01-05 15:03:23 +0800

    nina

---
+++ nina.js
@@ -0,0 +1,24 @@
+let fName = "Anin"
+console.log(fName)
+
+let myAge = 22
+console.log(myAge)
+
+let isMarried = myAge
+if(isMarried > 100){
+    console.log("SINGLE")
+}
+else{
+    console.log("Complicated")
+    }
+
+let cmHeight = 154
+var mheight  = 100
+console.log(cmHeight / mheight)
+
+let Fname = "Anin"
+
+console.log(Fname+myAge)
+
+console.log(myAge + 2023)
+console.log(myAge * 2024)
